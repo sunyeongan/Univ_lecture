c238fa5d77d01e106f8eadd03952e5789debeb1d
sunyeongan <asy100@naver.com>
2022-02-22 13:06:20 +0900

    push NLP

---
+++ 2.file_read/pdf.py
@@ -0,0 +1,12 @@
+from PyPDF2 import PdfFileReader
+
+def getTextPDF(pdfFileName, password =''):
+    pdf_file = open(pdfFileName, 'rb')
+    print('getTextPDF 입장')
+    read_pdf = PdfFileReader(pdf_file)
+    if password !='':
+        read_pdf.decrypt(password)
+    text = []
+    for i in range(0, read_pdf.getNumPages()):
+        text.append(read_pdf.getPage(i).extractText())
+    return '\n'.join(text)
